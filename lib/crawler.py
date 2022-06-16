9e83538304d57197c15382e9d5b2b6bfd2762761
Snejana <snezhanapirdopska@gmail.com>
2022-06-16 11:21:14 +0300

    Commit SP

--- lib/crawler.py
+++ lib/crawler.py
@@ -1,1 +1,14 @@
-print('Hello')
+import requests
+
+class Crawler:
+    def __init__(self, base_url = "https://www.nasdaq.com/market-activity/stocks/snejf/historical"):
+        self.base_url = base_url
+
+    def get_html(self):
+        r = requests.get(self.base_url)
+        if r.ok:
+            return r.text
+        else:
+            raise Exception("Server not found")
+
+

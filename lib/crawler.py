e61c1df0ce279a5b314a8ed99d916395b1dbad79
Snejana <snezhanapirdopska@gmail.com>
2022-06-18 22:10:06 +0300

    Commit SP

--- lib/crawler.py
+++ lib/crawler.py
@@ -4,12 +4,15 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 class Crawler:
-    def __init__(self, base_url = "https://www.nasdaq.com/market-activity/stocks/aapl/historical"):
+    def __init__(self, base_url = 'https://www.nasdaq.com/market-activity/stocks/aapl/historical'):
         self.base_url = base_url
 
     def get_html(self):
-        r = requests.get(self.base_url)
+        print(self.base_url)
+        r = requests.get('https://www.nasdaq.com/market-activity/stocks/aapl/historical')
+        print(r.status_code)
         if r.ok:
+
             return r.text
         else:
             raise Exception("Server not found")
@@ -18,11 +21,14 @@ class Crawler:
         soup = bs(html, 'html.parser')
         shares = soup.find('tbody', 'historical-data__table-body')
         print(shares)
+        return shares
 
     def start(self):
+        print("start")
         html = self.get_html()
         self.html = html
-        self.get_shares(html)
+        print(html)
+        #self.get_shares(html)
 
 
 if __name__ == '__main__':
